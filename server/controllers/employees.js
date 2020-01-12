1c84a4287ad7ba1292062a97e160b2d1f6fd471f
Olivier <habimanaolivier6@gmail.com>
2020-01-12 08:43:26 -0800

    Send emal to Registerd Employees

--- server/controllers/employees.js
+++ server/controllers/employees.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import isValid from "validator";
+import nodemailer from "nodemailer";
 import hashPassword from "../helpers/hashPassword";
 import { isForOfStatement } from "@babel/types";
 
@@ -31,11 +32,35 @@ class Employee {
       req.body.status,
       req.body.position
     ];
-    console.log(values);
     const text = `INSERT INTO employees(employee_name, na_id, phone, email, d_birth, password, status, position) VALUES($1, $2, $3, $4, $5, $6, $7, $8) returning *`;
     try {
       const { rows } = await pool.query(text, values);
-      //   const data = await token(rows[0]);
+
+      var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "3cf4b9838e4a5a",
+          pass: "0ebb6216f7395e"
+        }
+      });
+
+      var mailOptions = {
+        from: '"Task Force" <info@gocloud.com>',
+        to: rows[0].email,
+        subject: "Your Account Information ",
+        text: "Conglaturation to join our Company",
+        html: "<h1>Task Force Compnay</h1><p>Your Account is activated!</p>"
+      };
+      transport.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
+          return error;
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+
       return res
         .status(201)
         .send({ status: 201, message: "Employee created successfully" });
